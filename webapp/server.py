c3978e3b05fb8f35caf4d39f94a0005c89d526d7
Gianluca Truda <gianlucatruda@gmail.com>
2018-12-06 19:05:30 +0200

    Updated JS logic for webapp to play complete games (with partial API linkage so far). Updated webapp server to implement further HTTP requests functionality.

--- webapp/server.py
+++ webapp/server.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 API = 'http://127.0.0.1:8001'
 
-@app.route('/game/<game_id>/')
+@app.route('/game/<game_id>/', methods=["GET"])
 def board(game_id):
+    board = get_board(game_id)
+    return render_template('index.html', gameid=game_id, board=board)
 
+@app.route('/game/<game_id>/', methods=["POST"])
+def button(game_id):
+    board = get_board(game_id)
+    print("button pressed")
+    if request.method == 'POST':
+        return render_template('index.html', gameid=game_id, board=board)
+
+def get_board(game_id):
     r = requests.get(API+'/game/'+game_id)
     game_state = r.json()['games'][0][1]
     board = state_to_2d_list(game_state)
-
-    return render_template('index.html', gameid=game_id, board=board)
+    return board
 
 def state_to_2d_list(game_state:str):
     assert(len(game_state)==9)
@@ -22,3 +31,4 @@ def state_to_2d_list(game_state:str):
 
 if __name__ == '__main__':
     app.run(port='8002')
+    pieceplaced = 0
--- webapp/static/logic.js
+++ webapp/static/logic.js
@@ -1,17 +1,71 @@
 function clearBoard() {
     var cells = document.getElementsByClassName("cell");
     for (let i=0; i<cells.length; i++) {
-        cells[i].innerHTML = " ";
+        cells[i].innerHTML = ".";
     }
     console.log("Board cleared");
 }
 
-function newGame(cells) {
-    clearBoard(cells);
+function newGame() {
+    clearBoard();
+    document.getElementsByClassName("min-header")[0].innerHTML = "Minimal";
+    GAMEOVER = false;
 }
 
-function loadGame() {
-    alert('Load Game');
+function endGame(winner, triple) {
+    triple.forEach(element => {
+        document.getElementsByClassName("cell")[element-1].innerHTML = "<b>"+winner+"</b>";
+    });
+    GAMEOVER = true;
+    document.getElementsByClassName("min-header")[0].innerHTML = winner+"'s win!";
+}
+
+function opponentPlay(cells, id, game, piece) {
+    var hasMoved = false;
+    while (!hasMoved){
+        var pos = Math.floor(Math.random()*8);
+        if (cells[pos].innerHTML == "."){
+            cells[pos].innerHTML = "x";
+            hasMoved = true;
+        }
+    }
+}
+
+function postData(url, data) {
+    return fetch(url, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        // headers: {
+        //     'Accept': 'application/json',
+        //     'Content-Type': 'application/json'
+        // },
+    })
+    .then(response => response.json());
+}
+
+function placePiece(cells, id, game, piece) {
+    if (cells[id].innerHTML == "." && !GAMEOVER){
+        fetch('http://127.0.0.1:8001/game/' + game)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                console.log(JSON.stringify(myJson));
+                board = myJson['games'][0][1];
+                board[id] = piece;
+                myData = { state: board };
+                postData('http://127.0.0.1:8001/game/' + game, myData).then(
+                    data => console.log(JSON.stringify(data))
+                ).catch(error => console.error(error));
+                cells[id].innerHTML = piece;
+                if (!checkForWin(cells)) {
+                    opponentPlay(cells, id, game, piece);
+                    checkForWin(cells);
+                }
+
+            });
+    }
+
 }
 
 function checkForWin(cells) {
@@ -27,8 +81,8 @@ function checkForWin(cells) {
             values.push(cells[parseInt(triple[j])-1].innerHTML)
         }
         // console.log(values);
-        if(values.every((val, i, arr) => val === arr[0])){
-            console.log(values);
+        if (!values.includes('.') && values.every((val, i, arr) => val === arr[0])){
+            endGame(values[0], triple);
             return true
         }
     }
@@ -39,8 +93,20 @@ function checkForWin(cells) {
 
 
 var newGameButton = document.getElementsByClassName("newgame")[0];
-var loadGameButton = document.getElementsByClassName("loadgame")[0];
-
 newGameButton.addEventListener("click", newGame);
-loadGameButton.addEventListener("click", loadGame);
 
+var cells = document.getElementsByClassName("cell");
+var GAMEOVER = false;
+var GAME = 0;
+var PIECE = 'o';
+
+// EventListeners don't play nicely with loops :(
+cells[0].addEventListener("click", function(){placePiece(cells, 0, GAME, PIECE)});
+cells[1].addEventListener("click", function(){placePiece(cells, 1, GAME, PIECE)});
+cells[2].addEventListener("click", function(){placePiece(cells, 2, GAME, PIECE)});
+cells[3].addEventListener("click", function(){placePiece(cells, 3, GAME, PIECE)});
+cells[4].addEventListener("click", function(){placePiece(cells, 4, GAME, PIECE)});
+cells[5].addEventListener("click", function(){placePiece(cells, 5, GAME, PIECE)});
+cells[6].addEventListener("click", function(){placePiece(cells, 6, GAME, PIECE)});
+cells[7].addEventListener("click", function(){placePiece(cells, 7, GAME, PIECE)});
+cells[8].addEventListener("click", function(){placePiece(cells, 8, GAME, PIECE)});
