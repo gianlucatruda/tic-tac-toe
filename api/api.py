c940e7f75467f8ab2a869dbdbca8e67bf10dbb3b
Gianluca Truda <gianlucatruda@gmail.com>
2018-12-06 19:32:39 +0200

    Streamlined API dependencies.

--- api/api.py
+++ api/api.py
@@ -1,13 +1,12 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
 from json import dumps
 import sqlite3
 from flask_cors import CORS
 
 app = Flask(__name__)
-api = Api(app)
 CORS(app)
+api = Api(app)
 
 class GameState(Resource):
     def get(self, game_id):
