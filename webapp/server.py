19d303cc227bfcde3d050c4a854d0e2d7bbfc7ab
Gianluca Truda <gianlucatruda@gmail.com>
2018-12-06 14:14:24 +0200

    Added ability for webapp to load game data with a GET request to the API. Also updated the board template so that the webapp can display that game data on the board.

--- webapp/server.py
+++ webapp/server.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+API = 'http://127.0.0.1:8001'
 
+@app.route('/game/<game_id>/')
+def board(game_id):
 
-@app.route('/')
-def homepage():
-    return render_template('index.html', gameid='none')
+    r = requests.get(API+'/game/'+game_id)
+    game_state = r.json()['games'][0][1]
+    board = state_to_2d_list(game_state)
+
+    return render_template('index.html', gameid=game_id, board=board)
+
+def state_to_2d_list(game_state:str):
+    assert(len(game_state)==9)
+    board = [[],[],[]]
+    for key, c in enumerate(game_state):
+        board[key//3].append(c.lower())
+    return board
 
 if __name__ == '__main__':
-    import os
-    print("DIR:", os.getcwd())
     app.run(port='8002')
